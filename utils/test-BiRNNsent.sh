3ae0e92ec7e35b7b365dd98b7ac00e6695be7a56
davidwilby <d.wilby@sheffield.ac.uk>
2019-10-11 10:59:15 +0100

    modify test value comparison

--- utils/test-BiRNNsent.sh
+++ utils/test-BiRNNsent.sh
@@ -6,7 +6,7 @@ model_type=EncSent
 model_name=${task_name}_srcmt_${model_type}
 store_path=test_models/${model_name}/
 patience=5
-TESTVAL=0.1910756629756321 #PCC test value for sentence level, theano, BiRNN
+TESTVAL=0.191076 #PCC test value for sentence level, theano, BiRNN
 
 python utils/getTestData.py
 
@@ -14,8 +14,9 @@ PYTHONHASHSEED=0 python main.py || true > log-${model_name}-test.txt
 
 PCC=$(awk -F, 'NR==1 {next};$3>M {M=$3};END {print M}' trained_models/${task_name}_srcmt_${model_type}/val.qe_metrics)
 
+echo "PCC test value was $PCC which is equal to $TESTVAL"
+
 if [ "$PCC" = "$TESTVAL" ]; then
   echo "QE test passed (sentence level BiRNN with Theano on $task_name test dataset)"
-  echo "PCC test value was $PCC which is equal to $TESTVAL"
   exit 0
 fi
