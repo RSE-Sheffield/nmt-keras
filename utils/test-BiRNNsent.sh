064527371be8b36505f7831172cf1210bfa8a23b
davidwilby <d.wilby@sheffield.ac.uk>
2019-10-15 14:35:51 +0100

    add tensorflow testing to travis yaml

--- utils/test-BiRNNsent.sh
+++ utils/test-BiRNNsent.sh
@@ -6,18 +6,26 @@ model_type=EncSent
 model_name=${task_name}_srcmt_${model_type}
 store_path=test_models/${model_name}/
 patience=5
-TESTVAL=0.1910756629756321 #PCC test value for sentence level, theano, BiRNN
 
-python utils/getTestData.py
+if [ $KERAS_BACKEND = "theano" ] ; then
+  TESTVAL=0.1910756629756321 #PCC test value for sentence level, theano, BiRNN
+elif [ $KERAS_BACKEND = "tensorflow" ] ; then
+  TESTVAL=0.16354782921305733 #PCC test value for sentence level, theano, BiRNN
+else
+  echo "Keras backend environment variable incorrectly set"
+  exit 1
+fi
 
-PYTHONHASHSEED=0 python main.py || true > log-${model_name}-test.txt
+# python utils/getTestData.py
+#
+# PYTHONHASHSEED=0 python main.py || true > log-${model_name}-test.txt
 
 PCC=$(awk -F, 'NR==1 {next};$3>M {M=$3};END {print M}' trained_models/${task_name}_srcmt_${model_type}/val.qe_metrics)
 
 echo "PCC test value was $PCC tested against $TESTVAL"
 
 if echo $PCC $TESTVAL | awk '{exit ($1-$2)^2>1E-12}'; then
-  echo "QE test passed (sentence level BiRNN with Theano on $task_name test dataset)"
+  echo "QE test passed (sentence level BiRNN with $KERAS_BACKEND on $task_name test dataset)"
 else
-  echo "QE test failed (sentence level BiRNN with Theano on $task_name test dataset)"
+  echo "QE test failed (sentence level BiRNN with $KERAS_BACKEND on $task_name test dataset)"
 fi
