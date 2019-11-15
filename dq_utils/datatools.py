640fcc474c7ffaaa748950abf1c335bc4fdfa25b
davidwilby <d.wilby@sheffield.ac.uk>
2019-11-15 10:40:17 +0000

    python improvements ala drj

--- dq_utils/datatools.py
+++ dq_utils/datatools.py
@@ -18,42 +18,40 @@ def preprocessDoc(params):
                 write_path = filename + '_docProcess' + file_extension
                 copyfile(scores_file, write_path)
             elif ext != params['PRED_SCORE']:
-                if params['TEXT_FILES'].get(split) is not None:
-                    annotations_list = params['DATA_ROOT_PATH'] + '/' + params['TEXT_FILES'][split] + ext
+                if not params['TEXT_FILES'].get(split):
+                    continue
+                annotations_list = os.path.join(params['DATA_ROOT_PATH'], params['TEXT_FILES'][split] + ext)
 
-                    sentences = []
-                    sentences2d = []
-                    if isinstance(annotations_list, str) and os.path.isfile(annotations_list):
-                        with open(annotations_list, 'r') as list_:
-                            sentences_doc = []
-                            sent_counter = 0
-                            for line in list_:
-                                new_line = line.rstrip('\n')
-                                if (new_line == '#doc#'):
-                                    if sent_counter < doc_size:
-                                        for i in range(doc_size - sent_counter):
-                                            sentences_doc.append('<pad> <pad> <pad>')
-                                    sentences2d.append(sentences_doc)
-                                    sentences_doc = []
-                                    sent_counter = 0
-                                else:
-                                    if sent_counter < doc_size:
-                                        sentences.append(new_line)
-                                        sentences_doc.append(new_line)
-                                        sent_counter += 1
-                    elif isinstance(annotations_list, list):
-                        sentences = annotations_list
-                    else:
-                        raise Exception(
-                            'Wrong type for "annotations_list". It must be a path to a text file with the sentences or a list of sentences. '
-                            'It currently is: %s' % (str(annotations_list)))
-                    
-                    filename, file_extension = os.path.splitext(annotations_list)
-                    write_path = filename + '_docProcess' + file_extension
-                    with open(write_path, "w") as f:
-                        for i in range(0,len(sentences2d)):
-                            f.writelines(sentences2d[i])
-                            f.write('\n')
+                sentences = []
+                sentences2d = []
+                if isinstance(annotations_list, str) and os.path.isfile(annotations_list):
+                    with open(annotations_list, 'r') as list_:
+                        sentences_doc = []
+                        sent_counter = 0
+                        for line in list_:
+                            new_line = line.rstrip('\n')
+                            if new_line == '#doc#':
+                                for i in range(sent_counter,doc_size):
+                                    sentences_doc.append('<pad> <pad> <pad>')
+                                sentences2d.append(sentences_doc)
+                                sentences_doc = []
+                                sent_counter = 0
+                            else:
+                                if sent_counter < doc_size:
+                                    sentences.append(new_line)
+                                    sentences_doc.append(new_line)
+                                    sent_counter += 1
+                elif isinstance(annotations_list, list):
+                    sentences = annotations_list
+                else:
+                    raise Exception(
+                        'Wrong type for "annotations_list". It must be a path to a text file with the sentences or a list of sentences. '
+                        'It currently is: %s' % (str(annotations_list)))
+
+                filename, file_extension = os.path.splitext(annotations_list)
+                write_path = filename + '_docProcess' + file_extension
+                with open(write_path, "w") as f:
+                    for lines in sentences2d: f.writelines(lines); f.write('\n')
         params['TEXT_FILES'].update({split: os.path.basename(filename) + '_docProcess.'})
 
     return params
