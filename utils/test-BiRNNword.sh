4a7c4d568b48748acf1fc731af2f371d0fe9e2fb
davidwilby <d.wilby@sheffield.ac.uk>
2020-01-14 08:38:14 +0000

    update tests to use __main__.py

--- utils/test-BiRNNword.sh
+++ utils/test-BiRNNword.sh
@@ -16,7 +16,7 @@ TESTVAL=$(awk -v backend=$KERAS_BACKEND -v level=$level -v task_name=$task_name 
 
 python utils/getTestData_BiRNNword.py
 
-PYTHONHASHSEED=0 python train.py -c ${conf} TASK_NAME=$task_name DATASET_NAME=$task_name MODEL_TYPE=$model_type MODEL_NAME=$model_name || true > log-${model_name}-test.txt
+PYTHONHASHSEED=0 python __main__.py train -c ${conf} TASK_NAME=$task_name DATASET_NAME=$task_name MODEL_TYPE=$model_type MODEL_NAME=$model_name || true > log-${model_name}-test.txt
 
 PCC=$(awk -F, '/f1_prod/ {M=-1;next};$2>M {M=$2};END {print M}' trained_models/${task_name}_srcmt_${model_type}/val.qe_metrics)
 
@@ -30,7 +30,7 @@ else
   TRAIN_RESULT="passed"
   echo "QE training $TRAIN_RESULT ($level  level BiRNN with $KERAS_BACKEND on $task_name test dataset)"
   EPOCH=$(awk -F, '/f1_prod/ {M=-1;next};$2>M {M=$2;E=$1};END {print E}' trained_models/${task_name}_srcmt_${model_type}/val.qe_metrics)
-  PYTHONHASHSEED=0 python predict.py --dataset datasets/Dataset_${task_name}_srcmt.pkl --model trained_models/${model_name}/epoch_${EPOCH}.h5 --save_path saved_predictions/prediction_${task_name}/ --evalset test
+  PYTHONHASHSEED=0 python __main__.py predict --dataset datasets/Dataset_${task_name}_srcmt.pkl --model trained_models/${model_name}/epoch_${EPOCH}.h5 --save_path saved_predictions/prediction_${task_name}/ --evalset test
   PCC=$(awk -F, '/f1_prod/ {M=-1;next};$2>M {M=$2};END {print M}' saved_predictions/prediction_${task_name}/test.qe_metrics)
   TESTVAL=$(awk -v backend=$KERAS_BACKEND -v level=${level}Predict -v task_name=$task_name -v metric=$metric -F,\
    'NR==1 {next};$1==backend && $2==level && $3==task_name && $4==metric {M=$5};END {print M}' utils/testVals.csv )
