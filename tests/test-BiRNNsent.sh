c3dac840689ac0a7c7a28c105c9c6a6ad21eafed
davidwilby <d.wilby@sheffield.ac.uk>
2020-02-05 11:10:22 +0000

    rename 'dq_tests' directory to 'tests'

--- tests/test-BiRNNsent.sh
+++ tests/test-BiRNNsent.sh
@@ -2,23 +2,23 @@
 
 set -e
 
-conf=dq_tests/config-word-BiRNN.yml
-task_name=testData-word
-model_type=EncWord
+conf=tests/config-sent-BiRNN.yml
+task_name=testData-sent
+model_type=EncSent
 model_name=${task_name}_srcmt_${model_type}
 store_path=trained_models/${model_name}/
-level=word
-metric=f1_prod
+level=sentence
+metric=pearson
 
-# look in dq_tests/testVals.csv for the correct value to compare deepquest test run against
+# look in tests/testVals.csv for the correct value to compare deepquest test run against
 TESTVAL=$(awk -v backend=$KERAS_BACKEND -v level=$level -v task_name=$task_name -v metric=$metric -F,\
- 'NR==1 {next};$1==backend && $2==level && $3==task_name && $4==metric {M=$5};END {print M}' dq_tests/testVals.csv )
+ 'NR==1 {next};$1==backend && $2==level && $3==task_name && $4==metric {M=$5};END {print M}' tests/testVals.csv )
 
-python dq_tests/getTestData_BiRNNword.py
+python tests/getTestData_BiRNNsent.py
 
 python __main__.py train -c ${conf} TASK_NAME=$task_name DATASET_NAME=$task_name MODEL_TYPE=$model_type MODEL_NAME=$model_name || true > log-${model_name}-test.txt
 
-PCC=$(awk -F, '/f1_prod/ {M=-1;next};$2>M {M=$2};END {print M}' trained_models/${task_name}_srcmt_${model_type}/val.qe_metrics)
+PCC=$(awk -F, '/pearson/ {M=-1;next};$3>M {M=$3};END {print M}' trained_models/${task_name}_srcmt_${model_type}/val.qe_metrics)
 
 echo "PCC test value was $PCC tested against $TESTVAL"
 
@@ -29,11 +29,11 @@ if echo $PCC $TESTVAL | awk '{exit ($1-$2)^2<1E-12}'; then
 else
   TRAIN_RESULT="passed"
   echo "QE training $TRAIN_RESULT ($level  level BiRNN with $KERAS_BACKEND on $task_name test dataset)"
-  EPOCH=$(awk -F, '/f1_prod/ {M=-1;next};$2>M {M=$2;E=$1};END {print E}' trained_models/${task_name}_srcmt_${model_type}/val.qe_metrics)
+  EPOCH=$(awk -F, '/pearson/ {M=-1;next};$3>M {M=$3;E=$1};END {print E}' trained_models/${task_name}_srcmt_${model_type}/val.qe_metrics)
   python __main__.py predict --dataset ${store_path}/Dataset_${task_name}_srcmt.pkl --model trained_models/${model_name}/epoch_${EPOCH}.h5 --save_path saved_predictions/prediction_${task_name}/ --evalset test
-  PCC=$(awk -F, '/f1_prod/ {M=-1;next};$2>M {M=$2};END {print M}' saved_predictions/prediction_${task_name}/test.qe_metrics)
+  PCC=$(awk -F, '/pearson/ {M=-1;next};$3>M {M=$3};END {print M}' saved_predictions/prediction_${task_name}/test.qe_metrics)
   TESTVAL=$(awk -v backend=$KERAS_BACKEND -v level=${level}Predict -v task_name=$task_name -v metric=$metric -F,\
-   'NR==1 {next};$1==backend && $2==level && $3==task_name && $4==metric {M=$5};END {print M}' dq_tests/testVals.csv )
+   'NR==1 {next};$1==backend && $2==level && $3==task_name && $4==metric {M=$5};END {print M}' tests/testVals.csv )
   echo "PCC test value was $PCC tested against $TESTVAL"
   if echo $PCC $TESTVAL | awk '{exit ($1-$2)^2<1E-12}'; then
      PREDICT_RESULT="failed"
--- tests/test-master.sh
+++ tests/test-master.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [ "$TEST_MODE" = "sentence" ]; then
-  ./dq_tests/test-BiRNNsent.sh
+  ./tests/test-BiRNNsent.sh
 elif [ "$TEST_MODE" = "document" ]; then
-  ./dq_tests/test-BiRNNdoc.sh
+  ./tests/test-BiRNNdoc.sh
 elif [ "$TEST_MODE" = "word" ]; then
-  ./dq_tests/test-BiRNNword.sh
+  ./tests/test-BiRNNword.sh
 else
   exit 1
 fi
