127cdf4e188dfbd38a0cfc14845d88bfa72572aa
davidwilby <d.wilby@sheffield.ac.uk>
2020-02-13 14:52:41 +0000

    suppress future warnings in numpy for cli

--- deepquest/__init__.py
+++ deepquest/__init__.py
@@ -1,3 +1,7 @@
-from .train import main as train
-from .predict import main as predict
-from .score import main as score
+import warnings
+warnings.filterwarnings('ignore',category=FutureWarning)
+# Used to suppress numpy FutureWarnings, turn off in developer mode
+
+from deepquest.train import main as train
+from deepquest.predict import main as predict
+from deepquest.score import main as score
