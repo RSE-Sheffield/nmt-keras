66595e87b284173bb29975f27003f65830c91f68
davidwilby <d.wilby@sheffield.ac.uk>
2020-02-13 11:10:58 +0000

    add dq train help option

--- deepquest/__main__.py
+++ deepquest/__main__.py
@@ -72,6 +72,7 @@ def main():
     # parser for training
     train_parser = subparsers.add_parser('train', help='Train QE models')
     train_parser.set_defaults(func=train)
+    train_parser.add_argument("help", nargs='?', help="Show the help information.")
     train_parser.add_argument("-c", "--config",   required=False,
                               help="Config YAML or pkl for loading the model configuration. ")
     train_parser.add_argument("changes", nargs="*", help="Changes to config. "
@@ -81,9 +82,10 @@ def main():
     # parser for prediction
     predict_parser = subparsers.add_parser('predict', help='Sample using trained QE models')
     predict_parser.set_defaults(func=predict)
-    predict_parser.add_argument("--model", required=True,
+    predict_parser.add_argument("help", nargs='?', help="Show the help information.")
+    predict_parser.add_argument("--model", required=False,
                                 help="model file (.h5) to use")
-    predict_parser.add_argument("--dataset", required=True,
+    predict_parser.add_argument("--dataset", required=False,
                                 help="dataset file (.pkl) to use")
     predict_parser.add_argument("--save_path", required=False, help="Directory path to save predictions to. "
                                 "If not specified, defaults to STORE_PATH")
@@ -96,15 +98,23 @@ def main():
     # parser for scoring
     score_parser = subparsers.add_parser(
         'score', help='Evaluate a set of predictions with regard to a reference set. ')
+    score_parser.add_argument("help", nargs='?', help="Show the help information.")
     score_parser.set_defaults(func=score)
-    score_parser.add_argument(
-        "files", nargs=2, help="Two text files containing predictions and references. ")
+    score_parser.add_argument("files", nargs="*", help="Two text files containing predictions and references. ", default="")
 
     args = parser.parse_args()
+
+    print(sys.argv)
+
     if (not hasattr(args,'func')) or (len(sys.argv) == 1):
         parser.print_help()
-    elif hasattr(args,'func') and (len(sys.argv) == 2):
+    elif hasattr(args, 'func') and (len(sys.argv) == 2):
         parser.print_help()
+    elif hasattr(args, 'func') and 'help' in sys.argv:
+        parsers = {'train': train_parser,
+                   'predict': predict_parser,
+                   'score': score_parser}
+        parsers[str(args.func.__name__)].print_help()
     else:
         args.func(args)
     sys.exit(0)
