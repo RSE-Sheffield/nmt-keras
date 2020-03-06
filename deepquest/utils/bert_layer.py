ec5b24a1bbe678c99b4e159bd25c968b12b342a5
Fred Blain <f.blain@sheffield.ac.uk>
2020-03-06 22:41:15 +0000

    refactoring BERT layer (more customizable in its use).

--- deepquest/utils/bert_layer.py
+++ deepquest/utils/bert_layer.py
@@ -1,80 +1,142 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Bertlayer.py
-#
-# Copyright (C) 2019 Frederic Blain (feedoo) <f.blain@sheffield.ac.uk>
-#
-# Licensed under the "THE BEER-WARE LICENSE" (Revision 42):
-# Fred (feedoo) Blain wrote this file. As long as you retain this notice you
-# can do whatever you want with this stuff. If we meet some day, and you think
-# this stuff is worth it, you can buy me a tomato juice or coffee in return
-#
 
-"""
-
-"""
-
-# from keras.layers import *
-from keras.engine.base_layer import Layer
+import logging
 
 import tensorflow as tf
 import tensorflow_hub as hub
 from tensorflow.keras import backend as K
-# import keras_wrapper.extra.bert_tokenization as tokenization
+
+from keras.engine.base_layer import Layer
+
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s] %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+logger = logging.getLogger(__name__)
+
 
 class BertLayer(Layer):
-    def __init__(self, max_seq_len=70, output_representation='sequence_output', trainable=True, **kwargs):
+    def __init__(self,
+            bert_path="https://tfhub.dev/google/bert_multi_cased_L-12_H-768_A-12/1",
+            max_seq_len=70,
+            pooling=None,
+            trainable=True,
+            fine_tune_n_layers=3,
+            tune_embeddings=False,
+            return_seq_output=False,
+            verbose=False,
+            name='bert_layer', **kwargs):
+
+            # output_representation='sequence_output', tune_embeddings=False, trainable=True, **kwargs):
         self.bert = None
+        self.bert_path = bert_path
+
+        '''
+        POOLING strategies:
+        - pooling==None, no pooling is applied and the output tensor has shape
+          [batch_size, seq_len, encoder_dim]. This mode is useful for solving
+          token level tasks.
+        - pooling==’mean’, the embeddings for all tokens are mean-pooled and
+          the output tensor has shape [batch_size, encoder_dim]. This mode is
+          particularly useful for sentence representation tasks.
+        - pooling==’cls’, only the vector corresponding to first ‘CLS’ token is
+          retrieved and the output tensor has shape [batch_size, encoder_dim].
+          This pooling type is useful for solving sentence-pair classification
+          tasks.
+        '''
+        self.pooling = pooling
+        if self.pooling not in ["cls", "mean", None]:
+            raise NameError(
+                    f"Undefined pooling type (must be either 'cls', 'mean', or \
+                    None, but is {self.pooling}"
+                    )
+
+        self.fine_tune_n_layers = fine_tune_n_layers
+        self.trainable = trainable
+        self.tune_embeddings = tune_embeddings
+
+        self.max_seq_len = max_seq_len
+        self.return_seq_output = return_seq_output
+
+        self.var_per_encoder = 16
+
+        self.verbose = verbose
+
         super(BertLayer, self).__init__(**kwargs)
 
-        self.bert_path="https://tfhub.dev/google/bert_multi_cased_L-12_H-768_A-12/1"
-        self.output_representation = output_representation
-        self.max_seq_len = max_seq_len
-        self.trainable = trainable
 
     def build(self, input_shape):
         self.bert = hub.Module(self.bert_path,
-                               trainable=True, name="{}_module".format(self.name))
+                               trainable=self.trainable, name="{}_module".format(self.name))
 
-        # Remove unused layers and set trainable parameters
-        if self.output_representation in ['sequence_output', 'cls_output']:
-            # self.trainable_weights += [var for var in self.bert.variables
-                                       # if not "/cls/" in var.name and not "/pooler/" in var.name]
-            self.trainable_weights += [var for var in self.bert.variables
-                                       if "layer_9" in var.name or "layer_10" in var.name or "layer_11" in var.name or "layer_12" in var.name]
-            # self.trainable_weights += [var for var in self.bert.variables
-                                       # if "layer_5" in var.name or "layer_6" in var.name or "layer_7" in var.name or "layer_8" in var.name or "layer_9" in var.name or "layer_10" in var.name or "layer_11" in var.name or "layer_12" in var.name]
-            self.trainable_weights += [var for var in self.bert.variables
-                                       if "embeddings/word_embeddings" in var.name]
-            # self.trainable_weights += [var for var in self.bert.variables
-                                       # if "/layer_" in var.name]
-            # self.trainable_weights += [var for var in self.bert.variables
-                                       # if "embeddings" in var.name]
-        else:
-            self.trainable_weights += [var for var in self.bert.variables
-                                       if not "/cls/" in var.name]
+        trainable_layers = []
+        if self.tune_embeddings:
+            trainable_layers.append("embeddings")
+
+        if self.pooling == "cls":
+            trainable_layers.append("pooler")
+
+        if self.fine_tune_n_layers > 0:
+            encoder_var_names = [var.name for var in self.bert.variables if 'encoder' in var.name]
+            n_encoder_layers = int(len(encoder_var_names) / self.var_per_encoder)
+            for i in range(self.fine_tune_n_layers):
+                trainable_layers.append(f"encoder/layer_{str(n_encoder_layers - 1 - i)}/")
+
+        # Add module variables to layer's trainable weights
+        for var in self.bert.variables:
+            if any([l in var.name for l in trainable_layers]):
+                self._trainable_weights.append(var)
+            else:
+                self._non_trainable_weights.append(var)
+
+        if self.verbose:
+            logger.info("*** BERT LAYER - TRAINABLE VARS ***")
+            for var in self._trainable_weights:
+                logger.info(var)
+            logger.info("***********************************")
+
         super(BertLayer, self).build(input_shape)
 
-    def call(self, x, mask=None):
-        inputs = dict(input_ids=x[0], input_mask=x[1], segment_ids=x[2])
 
-        if self.output_representation in ['sequence_output', 'cls_output']:
-            outputs = self.bert(inputs, as_dict=True, signature='tokens')['sequence_output']
-        else:
-            outputs = self.bert(inputs, as_dict=True, signature='tokens')['pooled_output']
+    def call(self, inputs, mask=None):
+        inputs = [K.cast(x, dtype="int32") for x in inputs]
+        input_ids, input_mask, segment_ids = inputs
 
-        if self.output_representation == 'cls_output':
-            return K.tf.squeeze(outputs[:, 0:1, :], axis=1)
+        bert_inputs = dict(
+            input_ids=input_ids, input_mask=input_mask, segment_ids=segment_ids
+        )
+
+        output = self.bert(inputs=bert_inputs, signature="tokens", as_dict=True)
+
+        if self.pooling == "cls":
+            pooled = output["pooled_output"]
         else:
-            return outputs
+            result = output["sequence_output"]
+
+            input_mask = tf.cast(input_mask, tf.float32)
+            mul_mask = lambda x, m: x * tf.expand_dims(m, axis=-1)
+            masked_reduce_mean = lambda x, m: tf.reduce_sum(mul_mask(x, m), axis=1) / (
+                    tf.reduce_sum(m, axis=1, keepdims=True) + 1e-10)
+
+            if self.pooling == "mean":
+                pooled = masked_reduce_mean(result, input_mask)
+            else:
+                pooled = mul_mask(result, input_mask)
+
+        if self.return_seq_output:
+            return_elements = [pooled, result]
+        else:
+            return_elements = pooled
+
+        return return_elements
+
 
     def compute_mask(self, inputs, mask=None):
         return None
 
     def compute_output_shape(self, input_shape):
-        if self.output_representation in ['pooled_output', 'cls_output']:
-            return (None, 768)
-        else:
+        if self.pooling == None:
             return (None, self.max_seq_len, 768)
+        else: #'mean' and 'cls'
+            return (None, 768)
 
