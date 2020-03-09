cf2c1e42dafa3ef8cbb1266d32be549955b08fb5
davidwilby <d.wilby@sheffield.ac.uk>
2020-03-09 09:34:44 +0000

    temp changes for testing

--- tests/test_BiRNN.py
+++ tests/test_BiRNN.py
@@ -107,15 +107,12 @@ def test_BiRNN_word():
 
 	assert (result - testVal)**2 < 1E-10 # check that output is within 1E-5 of the expected result
 
-
-
 	# PREDICTION TEST
 
 	model = 'trained_models/' + model_name +'/epoch_' + epoch + '.h5'
 	dataset = 'trained_models/' + model_name + '/Dataset_' + task_name + '_srcmt.pkl'
 	save_path = 'saved_predictions/prediction_' + task_name + '/'
 	evalset = 'test'
-	# with pytest.raises(SystemExit):
 	dq.predict(model=model, dataset=dataset, save_path=save_path, evalset=evalset)
 	result = getPredictedVal(save_path + 'test.qe_metrics', metric)
 	testVal = getTestVal(backend, level, 'predict', task_name, metric)
@@ -151,15 +148,12 @@ def test_BiRNN_sentence():
 
 	assert (result - testVal)**2 < 1E-10 # check that output is within 1E-5 of the expected result
 
-
-
 	# PREDICTION TEST
 
 	model = 'trained_models/' + model_name +'/epoch_' + epoch + '.h5'
 	dataset = 'trained_models/' + model_name + '/Dataset_' + task_name + '_srcmt.pkl'
 	save_path = 'saved_predictions/prediction_' + task_name + '/'
 	evalset = 'test'
-	# with pytest.raises(SystemExit):
 	dq.predict(model=model, dataset=dataset, save_path=save_path, evalset=evalset)
 	result = getPredictedVal(save_path + 'test.qe_metrics', metric)
 	testVal = getTestVal(backend, level, 'predict', task_name, metric)
@@ -202,9 +196,13 @@ def test_BiRNN_document():
 	dataset = 'trained_models/' + model_name + '/Dataset_' + task_name + '_srcmt.pkl'
 	save_path = 'saved_predictions/prediction_' + task_name + '/'
 	evalset = 'test'
-	# with pytest.raises(SystemExit):
 	dq.predict(model=model, dataset=dataset, save_path=save_path, evalset=evalset)
 	result = getPredictedVal(save_path + 'test.qe_metrics', metric)
 	testVal = getTestVal(backend, level, 'predict', task_name, metric)
 
 	assert (result - testVal)**2 < 1E-10 # check that output is within 1E-5 of the expected result
+
+if __name__ == '__main__':
+	test_BiRNN_word()
+	test_BiRNN_sentence()
+	test_BiRNN_document()
