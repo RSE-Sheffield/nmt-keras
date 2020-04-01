79b26e590f1e9d1f919861f123327bc6662a69a8
David Wilby <d.wilby@sheffield.ac.uk>
2020-04-01 13:41:30 +0100

    fix bug in model for multi gpu

--- deepquest/qe_models/birnn_word.py
+++ deepquest/qe_models/birnn_word.py
@@ -15,7 +15,6 @@
 """
 
 from .model import QEModel
-from keras.utils import multi_gpu_model
 from .utils import *
 from .layers import *
 
@@ -205,9 +204,3 @@ class encword(QEModel):
                 inputs=inputs,
                 outputs=[output_qe_layer]
                 )
-        
-        if params.get('N_GPUS', 1) > 1:
-            self.multi_gpu_model = multi_gpu_model(self.model, gpus=params['N_GPUS'])
-        else:
-            self.multi_gpu_model = None
-
